31f628cfaf251e9c0e04cd9995cba979565b72f7
FadhelAlmalki <almalkifadhel@gmail.com>
2026-02-18 13:50:49 +0300

    added my solution

---
+++ bonus.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a positive integer: "))
+
+sum = 0
+
+for number in range(1, n + 1):
+    if number % 2 == 0:
+        sum += number
+
+print(f"The sum of even numbers between 1 and {n} is {sum}.")
+
+
---
+++ range_function.py
@@ -0,0 +1,9 @@
+for number in range(45,211):
+    
+    if number == 100:
+        continue
+
+    if number == 205:
+        break
+
+    print(number)
